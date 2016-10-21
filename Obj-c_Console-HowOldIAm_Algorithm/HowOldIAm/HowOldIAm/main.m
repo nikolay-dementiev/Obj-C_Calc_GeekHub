2368e6f9eabcb168546d4eb8b8051dd7de66e78a
Dementiev Nikolay <dementievnikolay@gmail.com>
2016-10-21 11:38:42 +0300

    2. HowOldIAm_Algorithm - init

---
+++ Obj-c_Console-HowOldIAm_Algorithm/HowOldIAm/HowOldIAm/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HowOldIAm
+//
+//  Created by Nikolay Dementiev on 21.10.16.
+//  Copyright © 2016 mc373. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+    return 0;
+}
